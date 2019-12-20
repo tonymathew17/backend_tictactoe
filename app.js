3f73e875d036a54c9ccc7e84dfc01a0516c68bfc
Tony Mathew <tony.b.mathew@accenture.com>
2019-12-20 12:12:03 +0530

    Added functionality to refresh the board, handling cases when a tile is clicked multiple times

--- app.js
+++ app.js
@@ -5,17 +5,28 @@ const socket = require('socket.io');
 const helper = require('./helper');
 
 // API to check health of the server
-app.get('/healthCheck', function(req, res) {
+app.get('/healthCheck', function (req, res) {
     res.send('Connected!');
-})
+});
+
+app.get('/refreshBoard', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    helper.userClickedTiles.length = 0;
+    helper.computerClickedTiles.length = 0;
+    res.status(200).send('Memory cleared!');
+});
 
 var io = socket(server);
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log('Socket connection established!');
 
-    socket.on('tileClicked', function(tileClicked) {
+    socket.on('tileClicked', function (tileClicked) {
         console.log('User clicked tile ', tileClicked);
-        let square = helper.savePlayerMove(tileClicked);
+        let res = helper.savePlayerMove(tileClicked);
+        if (!Array.isArray(res)) {
+            socket.emit('messageFromServer', res);
+            return;
+        }
         let computerMove = helper.getComputerMove();
         console.log('Computer selected ', computerMove);
         socket.emit('messageFromServer', computerMove);
--- helper.js
+++ helper.js
@@ -1,14 +1,17 @@
 let userClickedTiles = [];
 let computerClickedTiles = [];
+let clickedTiles = [];
+let possibleClicks = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 /**@description This function saves the tile clicked by user */
 function savePlayerMove(tileClicked) {
     try {
         if (userClickedTiles.includes(tileClicked)) {
-            throw "This tile has already been clicked!"
+            throw new RangeError("This tile has already been clicked!");
         } else {
             userClickedTiles.push(tileClicked);
-            return (tileClicked * tileClicked);
+            clickedTiles.push(tileClicked);
+            return userClickedTiles;
         }
 
     } catch (error) {
@@ -19,14 +22,20 @@ function savePlayerMove(tileClicked) {
 /**@description This function gets the computer move based on the tile user clicked */
 function getComputerMove() {
     let tile = Math.floor(Math.random() * 9) + 1;
-    while (userClickedTiles.includes(tile) || computerClickedTiles.includes(tile)) {
-        tile = Math.floor(Math.random() * 9) + 1;;
+
+    while (userClickedTiles.includes(tile) || computerClickedTiles.includes(tile) &&
+        ((Array.from(new Set(clickedTiles)).length != 9) && (!possibleClicks.every(elem => clickedTiles.includes(elem))))) {
+        tile = Math.floor(Math.random() * 9) + 1;
+        console.log('Inside whiel loop');
     }
     computerClickedTiles.push(tile);
+    clickedTiles.push(tile);
     return tile;
 }
 
 module.exports = {
     savePlayerMove,
-    getComputerMove
+    getComputerMove,
+    userClickedTiles,
+    computerClickedTiles
 }
